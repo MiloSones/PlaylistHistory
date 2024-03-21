cdc3860eff0d996670ac0d09ca4bc8eadd755bfc
Milo Sones <102179233+MiloSones@users.noreply.github.com>
2024-03-21 20:03:18 +0000

    Add routes

--- web-server/src/routes/+page.svelte
+++ web-server/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Spotify daily playlists archiver</h1>
+
+<div>
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+</div>
---
+++ web-server/src/routes/playlist/+page.svelte
@@ -0,0 +1,1 @@
+<p>Display All playlists</p>
